e399a71a55a32cfe80839fa8449b0b21e45c83aa
Jan <janmeijer20@gmail.com>
2020-06-30 21:03:00 +0200

    updated title screen
    last details.

--- src/title_screen.c
+++ src/title_screen.c
@@ -796,8 +796,8 @@ void CB2_InitTitleScreen(void)
                                     | DISPCNT_OBJ_ON
                                     | DISPCNT_WIN0_ON
                                     | DISPCNT_OBJWIN_ON);
-        //m4aSongNumStart(MUS_TITLE3);
-		m4aSongNumStart(MUS_SATTOWER);
+        //m4aSongNumStart(MUS_TITLE3); //MUS_SATTOWER
+		m4aSongNumStart(MUS_PYRAMID_TOP);
         gMain.state = 5;
         break;
     case 5:
